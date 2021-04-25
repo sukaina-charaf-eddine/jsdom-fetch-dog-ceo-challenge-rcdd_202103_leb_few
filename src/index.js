b1ff85eceab18c75b8fdeedc8c2c1ba5737fbc1c
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-25 09:29:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+fetch("imgUrl")
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+  
+})
+fetch("breedUrl")
+.then(function(response) {
+  return response.json();
+})
+.then(function(json){
+  
+})
+
